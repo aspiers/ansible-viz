e49c536e06ba7587a260e36603339473c0a9090d
Alexis Lee <alexisl@hp.com>
2015-03-10 15:54:47 +0000

    huge mess

--- ansible-viz.rb
+++ ansible-viz.rb
@@ -24,20 +24,6 @@ def thing(dict, type, name, extra = {})
   it
 end
 
-def with_context(dict, it)
-  dict[:context] ||= []
-  dict[:context].push it
-  begin
-    yield it
-  rescue
-    puts "Context:"
-    dict[:context].each {|i| puts "  " + i[:type].to_s + ": " + i[:name]}
-    raise
-  end
-ensure
-  dict[:context].pop
-end
-
 def ls(path, default=nil)
   if !File.directory? path
     if default == nil
@@ -56,6 +42,8 @@ def yaml_slurp(*steps)
 end
 
 ########## LOAD DATA #############
+# TODO slurp all YAML up-front, then process
+# TODO tests...
 
 def load_data(options)
   dict = {}
@@ -72,36 +60,35 @@ def load_data(options)
     /.yml$/i === file.downcase
   }.map { |file| mk_playbook(dict, playbook_dir, file) }
 
-  dict
+  postprocess(dict)
 end
 
 def mk_role(dict, path, name)
   role = thing(dict, :role, name)
-  with_context dict, role do
-    # Load vars before tasks, so they're available in dict
-    vardir = File.join(path, name, "vars")
-    role[:vars] = ls(vardir, []).
-      find_all {|f| f =~ /\.yml$/ }.
-      flat_map {|f| mk_vars(dict, vardir, f) }.
-      uniq  # FIXME
 
-    taskdir = File.join(path, name, "tasks")
-    role[:tasks] = ls(taskdir, []).
-      find_all {|f| f =~ /\.yml$/ }.
-      map {|f| mk_task(dict, taskdir, f) }.
-      uniq  # FIXME
-
-    used_vars = role[:tasks].flat_map {|task| task[:used_vars] }.uniq
-    role[:unused_vars] = role[:vars].reject {|i| used_vars.include? i }
-
-    begin
-      meta = yaml_slurp(path, name, "meta", "main.yml")
-      role[:role_deps] = (meta['dependencies'] || []).
-        map {|dep| dep['role'] }.
-        map {|dep| thing(dict, :role, dep) }
-    rescue Errno::ENOENT
-    end
+  # Load roles which support this one, EG with vars
+  begin
+    meta = yaml_slurp(path, name, "meta", "main.yml")
+    role[:role_deps] = (meta['dependencies'] || []).
+      map {|dep| dep['role'] }.
+      map {|dep| mk_role(dict, path, dep) }
+  rescue Errno::ENOENT
+    role[:role_deps] = []
   end
+
+  # Load vars before tasks, so they're available in dict
+  vardir = File.join(path, name, "vars")
+  role[:vars] = ls(vardir, []).
+    find_all {|f| f =~ /\.yml$/ }.
+    flat_map {|f| mk_vars(dict, vardir, f) }.
+    uniq  # FIXME
+
+  taskdir = File.join(path, name, "tasks")
+  role[:tasks] = ls(taskdir, []).
+    find_all {|f| f =~ /\.yml$/ }.
+    map {|f| mk_task(dict, taskdir, f) }.
+    uniq  # FIXME
+
   role
 end
 
@@ -114,7 +101,17 @@ def mk_task(dict, path, file)
   long = "Task " + rolename + "::" + name
   task = thing(dict, :task, long, {:role => rolename, :label => name})
 
-  task[:used_vars] = find_vars(yaml_slurp(path, file)).map {|varname|
+  taskdata = yaml_slurp(path, file) || []
+  task[:facts] = taskdata.map {|i| i['set_fact'] }.compact.flat_map {|i|
+    if i.is_a? Hash
+      i.keys
+    else
+      [i.split("=")[0]]
+    end
+  }
+  task[:used_vars] = find_vars(taskdata).reject {|varname|
+    task[:facts].include? varname
+  }.map {|varname|
     mk_var(dict, rolename, varname)
   }.uniq
 
@@ -139,6 +136,7 @@ def find_vars(data)
         s
       }.
       flat_map {|s| s.split("|") }.
+      reject {|s| s =~ /\w+\.stdout/ }.
       map {|s| s.split(".")[0] }.
       map {|s| s.split("[")[0] }.
       map {|s| s.gsub(/[^[:alnum:]_-]/, '') }.
@@ -170,6 +168,9 @@ def mk_vars(dict, path, file)
 end
 
 def mk_var(dict, rolename, name)
+#  if name == "vagrant_plugins"
+#    1/0
+#  end
   long = "Var " + rolename + "::" + name
   thing(dict, :var, long, {:role => $1, :label => name})
 end
@@ -178,30 +179,43 @@ def mk_playbook(dict, path, file)
   name = file.sub(/.yml$/, '')
   playbook = thing(dict, :playbook, name)
   data = yaml_slurp(path, file)
-  with_context dict, playbook do
-    playbook[:roles] = (data[0]['roles'] || []).map {|role|
-      if role.instance_of? Hash
-        role = role['role']
-      end
-      # All roles should be loaded already
-      dict[:role][role]
-    }.uniq  # FIXME
+  playbook[:roles] = (data[0]['roles'] || []).map {|role|
+    if role.instance_of? Hash
+      role = role['role']
+    end
+    # All roles should be loaded already
+    dict[:role][role]
+  }.uniq  # FIXME
 
-    playbook[:tasks] = (data[0]['tasks'] || []).map {|task_h|
-      rel_path = task_h['include'].split(" ")[0]
-      file = File.basename(rel_path)
-      taskdir = File.dirname(File.join(path, rel_path))
-      mk_task(dict, taskdir, file)
-    }.compact.uniq  # FIXME
-  end
+  playbook[:tasks] = (data[0]['tasks'] || []).map {|task_h|
+    rel_path = task_h['include'].split(" ")[0]
+    file = File.basename(rel_path)
+    taskdir = File.dirname(File.join(path, rel_path))
+    mk_task(dict, taskdir, file)
+  }.compact.uniq  # FIXME
   playbook
+end
+
+def postprocess(dict)
+  dict[:role].each_value {|role|
+    role[:used_vars] = role[:tasks].flat_map {|task| task[:used_vars] }.uniq
+    role[:used_vars] += role[:role_deps].flat_map {|dep| dep[:used_vars] }
+    role[:facts] = role[:tasks].flat_map {|t| t[:facts] }
+    role[:facts] += role[:role_deps].flat_map {|dep| dep[:facts] }
+    role[:unused_vars] = (role[:vars] - role[:used_vars]).reject {|v|
+      role[:facts].include? v[:label]
+    }
+    role[:undefed_vars] = role[:used_vars] - role[:vars]
+  }
+
+  dict
 end
 
 
 ########## GRAPHIFY ###########
 
 def graphify(dict, options)
-  # FIXME All the cosmetic stuff should be factored out to decorate methods really
+  # FIXME Cosmetic stuff should be in decorate_* methods
 
   g = Graph.new
   g[:rankdir] = 'LR'
@@ -229,38 +243,34 @@ def graphify(dict, options)
 
   # Add edges from playbooks to roles and tasks
   dict[:playbook].each_value {|playbook|
-    with_context dict, playbook do
-      (playbook[:roles] || []).each {|role|
-        g.add GEdge[playbook[:node], role[:node],
-          {:tooltip => "includes"}]
-      }
-      (playbook[:tasks] || []).each {|task|
-        g.add GEdge[playbook[:node], task[:node],
-          {:style => 'dashed', :color => 'blue',
-           :tooltip => "calls task"}]
-      }
-    end
+    (playbook[:roles] || []).each {|role|
+      g.add GEdge[playbook[:node], role[:node],
+        {:tooltip => "includes"}]
+    }
+    (playbook[:tasks] || []).each {|task|
+      g.add GEdge[playbook[:node], task[:node],
+        {:style => 'dashed', :color => 'blue',
+         :tooltip => "calls task"}]
+    }
   }
 
   # Add edges from roles to other things
   dict[:role].each_value {|role|
-    with_context dict, role do
-      (role[:role_deps] || []).each {|dep|
-        g.add GEdge[role[:node], dep[:node],
-          {:color => 'hotpink',
-           :tooltip => "calls foreign task"}]
-      }
+    (role[:role_deps] || []).each {|dep|
+      g.add GEdge[role[:node], dep[:node],
+        {:color => 'hotpink',
+         :tooltip => "calls foreign task"}]
+    }
 
-      (role[:tasks] || []).each {|task|
-        g.add GEdge[role[:node], task[:node],
-          {:tooltip => "calls task"}]
-      }
+    (role[:tasks] || []).each {|task|
+      g.add GEdge[role[:node], task[:node],
+        {:tooltip => "calls task"}]
+    }
 
-      (role[:vars] || []).each {|var|
-        g.add GEdge[role[:node], var[:node],
-          {:tooltip => "provides var"}]
-      }
-    end
+    (role[:vars] || []).each {|var|
+      g.add GEdge[role[:node], var[:node],
+        {:tooltip => "provides var"}]
+    }
   }
 
   # Add edges from tasks to vars
@@ -268,13 +278,11 @@ def graphify(dict, options)
     if task[:node] == nil
       next
     end
-    with_context dict, task do
-      (task[:used_vars] || []).each {|var|
-        g.add GEdge[task[:node], var[:node],
-          {:style => 'dotted',
-           :tooltip => "uses var"}]
-      }
-    end
+    (task[:used_vars] || []).each {|var|
+      g.add GEdge[task[:node], var[:node],
+        {:style => 'dotted',
+         :tooltip => "uses var"}]
+    }
     }
 
   hide_tasks = dict[:task].each_value.find_all {|it|
@@ -282,11 +290,13 @@ def graphify(dict, options)
   }.map {|it| it[:node] }
   g.lowercut(*hide_tasks)
 
+  g = decorate(g, dict, options)
+
   if not options.show_vars
     g.cut(*(dict[:var].values.map {|it| it[:node] }))
   end
 
-  decorate(g, dict, options)
+  g
 end
 
 
@@ -311,17 +321,24 @@ def decorate(g, dict, options)
     end
   }
 
-  if options.show_vars
 #    dict[:role].values.each {|r|
 #      r[:node][:tooltip] = r[:unused_vars].map {|v| v[:label] }.join(" ")
 #    }
-    dict[:role].values.flat_map {|r| r[:unused_vars] }.
-        map {|v| v[:node] }.
-        each {|n|
-      n[:fillcolor] = 'yellow'
-      n[:tooltip] = '(EXPERIMENTAL) appears not to be used by any task in the owning role'
-    }
-  end
+  dict[:role].values.flat_map {|r| r[:unused_vars] }.
+      map {|v| v[:node] }.
+      each {|n|
+    n[:fillcolor] = 'yellow'
+    n[:tooltip] += '. (EXPERIMENTAL) appears not to be used by any task in the owning role'
+  }
+
+  dict[:role].values.flat_map {|r| r[:undefed_vars] }.compact. # FIXME compact
+      map {|v| v[:node] }.
+      each {|n|
+    n[:fillcolor] = 'red'
+    n[:tooltip] += '. (EXPERIMENTAL) not defined by this role;' +
+                  ' could be included from another role or not really a var'
+  }
+
   g
 end
 
@@ -356,7 +373,8 @@ OptionParser.new do |o|
   o.on("-o", "--output [FILE]", "Where to write output") do |fname|
     options.output_filename = fname
   end
-  o.on("--vars", "Include vars") do |val|
+  o.on("--vars",
+       "Include vars. WARNING: unused/undefined support is EXPERIMENTAL.") do |val|
     options.show_vars = true
   end
   o.on_tail("-h", "--help", "Show this message") do
