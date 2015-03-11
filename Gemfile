d607e86717176faa6e44a5333c93bc7af25c31a1
Alexis Lee <alexisl@hp.com>
2015-03-11 10:48:12 +0000

    Add SimpleCov unit test coverage

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'mustache', '0.99.4'
-gem 'simplecov'
+gem 'simplecov', :require => false, :group => :test
--- test-viz.rb
+++ test-viz.rb
@@ -1,16 +1,14 @@
 #!/usr/bin/ruby
 
-require 'test/unit'
-#require 'coverage.so'
-#Coverage.start
+require 'rubygems'
+require 'bundler/setup'
 
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/test"
+end
+
+require 'test/unit'
 require './test-loader'
 require './test-postprocessor'
 require './test-grapher'
-#Coverage.result.each_pair {|file, a|
-#  if file !~ /ansible-viz/ or file =~ /\/test/
-#    next
-#  end
-#  a = a.compact
-#  puts "#{file}: "+ (100 * a.inject(0.0) {|acc, i| acc + i} / a.length).to_i.to_s
-#}
