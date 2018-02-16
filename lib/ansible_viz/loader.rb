af0f61acfabf502ef8c21f198aac19ec28a2b2e9
Adam Spiers <ansible-viz@adamspiers.org>
2018-02-16 13:26:47 +0000

    avoid some warnings from Ruby
    These appeared when $VERBOSE is set, which seems to be done by minitest.

--- lib/ansible_viz/loader.rb
+++ lib/ansible_viz/loader.rb
@@ -11,7 +11,7 @@ require 'pp'
 require 'ansible_viz/utils'
 
 def thing(parent, type, name, path, extra = {})
-  human_path = path.sub /^#{ENV['HOME']}/, '~'
+  human_path = path.sub(/^#{ENV['HOME']}/, '~')
   debug 2, "Loading new #{type} '#{name}' from #{human_path}, parent '#{parent[:name]}'"
   it = {:type => type, :name => name, :fqn => name, :path => path}.merge(extra)
   if parent[:type] != nil
